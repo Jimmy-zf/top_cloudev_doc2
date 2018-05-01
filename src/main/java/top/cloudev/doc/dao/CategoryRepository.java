7786b9f9cd1a460be948e2708ec77071fff2a6ab
Jimmy-zf <584744577@qq.com>
2018-05-01 14:33:00 +0800

    调整Cagegory领域类的Dao访问接口

--- src/main/java/top/cloudev/doc/dao/CategoryRepository.java
+++ src/main/java/top/cloudev/doc/dao/CategoryRepository.java
@@ -13,9 +13,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //@RepositoryRestResource(path = "newpath")
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 
-    Page<Category> findByIsDeletedFalse(Pageable pageable);
+    Page<Category> findByProjectIdAndIsDeletedFalse(long projectId, Pageable pageable);
 
     // 标准查询
-    Page<Category> findByNameContainingAndIsDeletedFalseAllIgnoringCase(String name, Pageable pageable);
+    Page<Category> findByProjectIdAndNameContainingAllIgnoringCaseAndIsDeletedFalse(long projectId, String name, Pageable pageable);
 
 }
